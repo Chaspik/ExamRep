6df6add173dbadae03526eab45e23c4b8f129ea6
Минайлов Олег12 <Minaylov.oleg@yandex.ru>
2022-07-05 07:08:16 +0300

    Добавьте файлы проекта.

---
+++ Exam2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Exam2.Classes;
+using Exam2.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new PageAutoriz());
+            Administrator.MainFrame = MainFrame;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Administrator.MainFrame.GoBack();
+        }
+        private void Main_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                Button_Back.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Button_Back.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ Exam2/Pages/PageAutoriz.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exam2.Classes;
+using Exam2.Pages;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAutoriz.xaml
+    /// </summary>
+    public partial class PageAutoriz : Page
+    {
+        public PageAutoriz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((UserPasswordBox.Password.ToString() == "admin" && UserLogin.Text == "admin"))
+            {
+                MessageBox.Show("Добро пожаловать, " + UserLogin.Text);
+                Administrator.MainFrame.Navigate(new PageAdmin());
+            }
+            else if((UserPasswordBox.Password.ToString() == "user" && UserLogin.Text == "user"))
+            {
+                MessageBox.Show("Добро пожаловать, " + UserLogin.Text);
+                Administrator.MainFrame.Navigate(new PageCustomer());
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+
+            }
+        }
+    }
+}
